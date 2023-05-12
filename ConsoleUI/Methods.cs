55cc15f129600f34477cd483095095b2214eab06
Mohammad Jabir <77463773+Lock0NStrat0s@users.noreply.github.com>
2023-05-12 12:19:34 -0600

    code complete.

---
+++ ConsoleUI/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI;
+
+public static class Methods
+{
+    public static void SimpleMethod()
+    {
+        throw new InvalidOperationException("You should not be calling the SimpleMethod");
+    }
+
+    public static void DifferentMethod()
+    {
+        Console.WriteLine("This is the different method working properly.");
+    }
+
+    public static void ComplexMethod(string name)
+    {
+        if (name.ToLower() == "tim")
+        {
+            throw new InsufficientMemoryException("Tim is too tall for this method.");
+        }
+        else
+        {
+            throw new ArgumentException("This person is not Tim.");
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,1 +1,35 @@
 ﻿
+using ConsoleUI;
+
+string name = "";
+try
+{
+    Methods.DifferentMethod();
+
+    Console.Write("What is your name: ");
+    name = Console.ReadLine();
+    Methods.ComplexMethod(name);
+    Methods.SimpleMethod();
+}
+catch (InvalidOperationException ex)
+{
+    Console.WriteLine("You should not be calling these methods.");
+    Console.WriteLine(ex.Message);
+}
+catch (NotImplementedException)
+{
+    Console.WriteLine("You forgot to finish your code!");
+}
+catch (Exception) when (name.ToLower() == "tim")
+{
+    Console.WriteLine("You used Tim's name, didn't you?");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+    //Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("I always run");
+}
